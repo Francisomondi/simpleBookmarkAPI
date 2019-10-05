a12ceb25cb5693ab8317587836e251133e0a5f3d
francis omondi <francisomondi17@gmail.com>
2019-10-06 01:38:32 +0300

    added an event listener to my form inputs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert("this is my js file");
+myForm = document.getElementById('myForm');
+
+
+
+
+
+
+myForm.addEventListener( 'submit', (e) =>{
+    const siteName = document.getElementById('siteName').value;
+    const siteUrl = document.getElementById('siteName').value;
+
+     e.preventDefault();
+     bookmark = {
+         name:  siteName,
+         url: siteUrl
+     }
+    console.log(bookmark);
+}
+);
