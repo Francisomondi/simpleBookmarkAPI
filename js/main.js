3a998321cfbb1022f02e17d943ecb34b4a93d1f1
francis omondi <francisomondi17@gmail.com>
2019-10-06 11:21:11 +0300

    displaying content from my local storage to the frontend API

--- js/main.js
+++ js/main.js
@@ -31,13 +31,32 @@ myForm.addEventListener( 'submit', (e) =>{
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
-       
+     }
+     
+      
+   
+    
+}
+//fetching bokmarks
 
 
 
+);
+function fetchbookmarks(){
+    const bookmarks = JSON.parse(localStorage.getItem('bookmarks')); 
+    //get output results
+     const bookmarkresults = document.getElementById('bookmarkResults');
+     
+     //build output
+     bookmarkresults.innerHTML = '';
+     for(let i=0; i< bookmarks.length; i++){
+         let name = bookmarks[i].name;
+         let url = bookmarks[i].url;
+
+         bookmarkresults.innerHTML += '<div class="well">' + 
+                                        '<h3>' +name +
+                                         '</h3>' + 
+                                         '</div>'
 
      }
-    
-    console.log(bookmark);
-}
-);
+  }
